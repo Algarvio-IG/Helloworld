af72953a158c0f058bfb8f7df7491c67052708db
Algarvio-IG <73707129+Algarvio-IG@users.noreply.github.com>
2020-11-08 20:58:04 +0000

    JavaTester

---
+++ Helloworld/src/main/java/pt/iade/Helloworld/controllers/JavaTesterController.java
@@ -0,0 +1,32 @@
+package pt.iade.Helloworld.controllers;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping(path="/api/java/tester")
+public class JavaTesterController{
+    private Logger logger = LoggerFactory.getLogger(JavaTesterController.class);
+    @GetMapping(path = "/author", produces= MediaType.APPLICATION_JSON_VALUE)
+    public void getAuthor()
+    {
+        String nome="Gonçalo Felicino";
+        double altura=1.74;
+        int numero=20190884;
+        boolean is_Fan=true;
+        String clube="Benfica";
+        if(is_Fan){
+            return("Done by"+nome+"whit number"+numero+".\n"+"I am"+height+"tall and I am a fan of football."+".\n"+"My favourite club is"+clube+".");
+        }
+        else if(!is_Fan){
+            return("Done by"+nome+"whit number"+numero+".\n"+"I am"+height+"tall and I am a fan of football.");
+        }
+    }
+
+
+
+
+    
+}
